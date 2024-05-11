2fbd996b03391fa1f7294819edc2f8c43cd05781
Amy-038 <amy.sirma@student.moringaschool.com>
2024-05-11 13:58:18 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [n for n in num_list if n % 2 == 0]
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    with_exclamation = [n + "!" for n in sentence_list]
+    return with_exclamation
+
+
